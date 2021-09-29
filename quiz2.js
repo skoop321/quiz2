65ce36d7ceb4a0ce0c9716cf6d6f5d1caa15e6d1
skoop321 <skoop321@gmail.com>
2021-09-29 14:01:55 -0600

    adding files

---
+++ quiz2.js
@@ -0,0 +1,13 @@
+//Samuel Koop's Quiz 2 Javascript for Mrs. Lambey's Advanced Web 2021
+var z = document.getElementById("extra-text");
+  z.style.display = "none";
+
+function showMore() {
+  var y = document.getElementById("extra-text");
+    y.style.display = "block";
+}
+
+function showLess() {
+  var x = document.getElementById("extra-text");
+    x.style.display = "none";
+}
